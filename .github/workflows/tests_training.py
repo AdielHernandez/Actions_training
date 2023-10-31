22802060b231f461e7c18e5a0d7b4511c4924320
AdielHernandez <adiel.hernandez@uvltd.tech>
2023-10-31 10:50:11 -0600

    test2

---
+++ .github/workflows/tests_training.py
@@ -0,0 +1,9 @@
+from ...training import *
+
+list1 = [2,5,6,1]
+list_ans = [1,2,5,6]
+def test_sort():
+    assert sortArr(list1) == list_ans
+
+def test_sum():
+    assert sum(5,5) == 10
--- training.py
+++ training.py
@@ -1,1 +1,15 @@
-print("this is a tests, yes")
+
+def sortArr(arr1):
+    arr1.sort()
+    return arr1
+
+def sum(num1, num2):
+    total = num1 + num2
+    return total
+
+
+# arr = [2,31,3,5,1,4,12]
+# num1 = 4
+# num2 = 8
+
+# (sortArr(arr))
